432dea58a20e71c2b17622021aa36b03dd31996d
Mike Culbertson <explody@explody.io>
2015-12-01 18:14:26 -0800

    initial

---
+++ apeye/__init__.py
@@ -0,0 +1,121 @@
+import os
+import sys
+import yaml
+import logging
+from six import iteritems
+
+from .version import VERSION
+
+__version__ = VERSION
+
+DEFAULTS = {'scheme': 'https',
+            'port': 443}
+
+# We only do JSON APIs right now
+DEFAULT_HEADERS = {'Content-type': 'application/json',
+                   'Accept': 'application/json'}
+
+# Paths to check, in order
+#   calling script directory
+#   module directory
+#   user home directory
+confpaths = [os.path.dirname(os.path.realpath(sys.argv[0])),
+             os.path.dirname(os.path.realpath(__file__)),
+             os.path.expanduser('~')]
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class ApeyeException(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class ApeyeConfigNotFound(Exception):
+
+    def __init__(self, filename, paths):
+        self.value = "Not found: [" + filename + "] I looked here:" + \
+                     " ".join(paths)
+
+    def __str__(self):
+        return self.value
+
+
+class ApeyeConfig(object):
+
+    def __init__(self, conffile='apeye.yml', environ='development'):
+
+        if 'APEYE_ENV' in os.environ:
+            apeye_env = os.environ['APEYE_ENV']
+        else:
+            apeye_env = environ
+
+        self.confpath = self.find_config(conffile)
+        fh = open(self.confpath, "rb")
+        conf = yaml.load(fh.read())
+
+        # Add the directory of the file we just loaded to confpaths
+        self.confpaths = confpaths + [os.path.dirname(
+                                        os.path.abspath(self.confpath))]
+
+        if apeye_env not in conf['environments']:
+            raise ApeyeException("No such environment is configured: %s" %
+                                 apeye_env)
+
+        self._api_attrs = conf.get('common', {})
+        self.headers = DEFAULT_HEADERS
+
+        if 'headers' in conf:
+            self.headers.update(conf['headers'])
+
+        env_attrs = conf['environments'][apeye_env]
+
+        self.api_name = os.path.splitext(os.path.basename(self.confpath))[0]
+
+        self._api_attrs.update(env_attrs)
+
+        for k, v in iteritems(self._api_attrs):
+            setattr(self, k, v)
+
+        # noinspection PyUnresolvedReferences
+        if self.credfile:
+            self.credfile = self.find_config(self.credfile)
+            self.credentials = yaml.load(open(self.credfile, "rb").read())
+
+        # noinspection PyUnresolvedReferences
+        objfile = self.find_config(self.objfile)
+        ofh = open(objfile, "rb")
+        self.objdata = yaml.load(ofh.read())
+
+        # noinspection PyUnresolvedReferences
+        apifile = self.find_config(self.apifile)
+        afh = open(apifile, "rb")
+        self.apidata = yaml.load(afh.read())
+
+    def set(self, key, value):
+
+        setattr(self, key, value)
+
+    def get(self, key):
+
+        return getattr(self, key)
+
+    def find_config(self, filename):
+
+        # expanduser here in case someone passed a "~" path
+        filename = os.path.expanduser(filename)
+
+        if os.path.isfile(filename):
+            return os.path.abspath(filename)
+
+        for path in self.confpaths:
+            confpath = os.path.join(path, filename)
+            if os.path.isfile(confpath):
+                return confpath
+
+        raise ApeyeConfigNotFound(filename, self.confpaths)
---
+++ apeye/api.py
@@ -0,0 +1,278 @@
+
+import re
+import requests
+import simplejson as json
+from six import iteritems
+
+from . import ApeyeConfig
+
+
+class ApeyeObjectNotFound(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class ApeyeError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class ApeyeInvalidArgs(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class ApeyeApiClass(object):
+
+    def __init__(self):
+        self._methods = []
+
+    def __repr__(self):
+        return type(self).__name__
+
+    def register_method(self, name):
+        self._methods.append(name)
+
+    def methods(self):
+        return [getattr(self, mth) for mth in self._methods]
+
+
+class HTTPGenericHeaderAuth(requests.auth.AuthBase):
+
+    def __init__(self, headers):
+        self.headers = headers
+
+    def __call__(self, r):
+        r.headers.update(self.headers)
+        return r
+
+class HTTPGenericParameterAuth(requests.auth.AuthBase):
+
+    def __init__(self, params):
+        self.params = params
+
+    def __call__(self, r):
+        r.params.update(self.headers)
+        return r
+
+class ApeyeApiMethod(object):
+
+    def __init__(self, apiobj, clsobj, name, method_data):
+
+        self.name = name
+        self.api = apiobj
+        self.cls = clsobj
+        self.method_data = method_data
+
+        if 'public' in method_data:
+            self.public = method_data['public']
+        else:
+            self.public = False
+
+        self.method = method_data['method']
+        self.path = method_data['path']
+
+        # Here, we handle parameters defined as 'must' or 'may'
+        self.must = []
+        self.may = []
+        self.default_params = {}
+        self.default_tokens = {}
+
+        if hasattr(self.api.conf, 'params'):
+            self.may += self.api.conf.params.get('may',[])
+            self.must += self.api.conf.params.get('must',[])
+
+        if 'must' in self.method_data:
+            self.must = self.method_data['must']
+
+        if 'may' in self.method_data:
+            self.may += self.method_data['may']
+
+        # Start with default parameters for the api and method, if they exist
+        if hasattr(self.api.conf, 'default_params'):
+            self.default_params.update(self.api.conf.default_params)
+
+        if 'default_params' in self.method_data:
+            self.default_params.update(self.method_data['default_params'])
+            self.may += self.method_data['default_params']
+
+        # Start with default path tokens for the api and method, if they exist
+        if hasattr(self.api.conf, 'default_tokens'):
+            self.default_tokens.update(self.api.conf.default_tokens)
+
+        if 'default_tokens' in self.method_data:
+            self.default_tokens.update(self.method_data['default_tokens'])
+
+        self.may = list(set(self.may))
+        self.must = list(set(self.must))
+
+        self.url = "%s://%s:%s%s%s" % (self.api.conf.scheme,
+                                       self.api.conf.host,
+                                       self.api.conf.port,
+                                       self.api.conf.basepath,
+                                       method_data['path'])
+
+
+    def to_json(self):
+
+        return json.dumps({
+            'scheme': self.scheme,
+            'host': self.host,
+            'port': self.port,
+            'credentials': self.creds,
+            'url': self.url,
+            'method_data': self.method_data
+        })
+
+    def path_tokens(self):
+
+        tokenre = re.compile(':([a-zA-Z0-9_]+)')
+        return tokenre.findall(self.path)
+
+    def __repr__(self):
+
+        return '%s.%s.%s' % (self.api.conf.api_name, self.cls, self.name)
+
+    def __call__(self, **kwargs):
+
+        # This is where we can put validations on the kwargs,
+        # based off data in api.yml (not there yet)
+
+        url = self.url
+        data = {}
+        params = self.default_params.copy()
+        tokens = self.default_tokens.copy()
+
+        # Overwrite with provided arguments. Pop the value out of kwargs.
+        if 'params' in kwargs:
+            params.update(kwargs.pop('params'))
+
+        # No defaults for data.
+        if 'data' in kwargs:
+            data = kwargs.pop('data')
+
+        # The remaining kwargs *should* correspond to path tokens
+        # Merge with defaults, if they exist
+        tokens.update(kwargs)
+
+        # Ensure all our path tokens are accounted for
+        for tok in self.path_tokens():
+            if tok not in tokens:
+                raise ApeyeInvalidArgs("%s called with missing token "
+                                       "argument %s. For path %s" %
+                                       (self, tok, self.path))
+
+        # Presence of our path tokens is verified. Replace them in our url
+        for k, v in iteritems(tokens):
+            url = url.replace(':%s' % k, str(v))
+
+        try:
+            resp = None
+            while True:
+
+                if self.method == "GET":
+                    r = requests.get(url, headers=self.api.conf.headers,
+                                     auth=self.api.auth())
+                elif self.method == "POST":
+                    r = requests.post(url, data=json.dumps(data),
+                                      headers=self.api.conf.headers,
+                                      auth=self.api.auth())
+                elif self.method == "PUT":
+                    r = requests.put(url, data=json.dumps(data),
+                                     headers=self.api.conf.headers,
+                                     auth=self.api.auth())
+                elif self.method == "DELETE":
+                    r = requests.delete(url, data=json.dumps(data),
+                                        headers=self.api.conf.headers,
+                                        auth=self.api.auth())
+
+                if r.status_code == 404:
+                    raise ApeyeObjectNotFound("Object not found. Tried: %s. "
+                                              "Apeye says: %s" %
+                                              (url, r.json()))
+                elif r.status_code != 200:
+                    raise ApeyeError("ERROR at %s Apeye says: %s" %
+                                     (url, r.json()))
+
+                thisresp = r.json()
+
+                if isinstance(resp, list):
+                    resp.extend(thisresp)
+                elif isinstance(resp, dict):
+                    resp.update(thisresp)
+                else:
+                    resp = thisresp
+
+                if 'next' not in r.links:
+                    break
+                else:
+                    url = r.links['next']['url']
+
+        except requests.exceptions.HTTPError as e:
+            raise ApeyeError("ERROR: %s" % e)
+
+        return resp
+
+
+class ApeyeApi(object):
+
+    def __init__(self, **kwargs):
+
+        self.conf = ApeyeConfig(**kwargs)
+        self._classes = []
+
+        # loop through the classes in okta-api.yml
+        for cls, mths in iteritems(self.conf.apidata['classes']):
+      
+            # Each top level class name will be an ApeyeApiClass
+            new_type = type(cls, (ApeyeApiClass,), {})
+            new_cls = new_type()
+
+            # For each defined method, add an ApeyeApiMethod as
+            # an attribute in the ApeyeApiClass instance
+            for mth, data in iteritems(mths):
+                setattr(new_cls, mth, ApeyeApiMethod(self, new_cls, mth, data))
+                new_cls.register_method(mth)
+
+            setattr(self, cls, new_cls)
+            self._classes.append(cls)
+
+    def auth(self):
+
+        if self.conf.authtype == 'basic':
+
+            u = self.conf.credentials.get('username', None)
+            p = self.conf.credentials.get('password', None)
+            return requests.auth.HTTPBasicAuth(u, p)
+
+        elif self.conf.authtype == 'header':
+
+            return HTTPGenericHeaderAuth(self.conf.credentials)
+
+        elif self.conf.authtype == 'param':
+
+            return HTTPGenericParameterAuth(self.conf.credentials)
+
+    def classes(self):
+
+        return [getattr(self, cls) for cls in self._classes]
+
+    def methods(self):
+        mths = []
+        for cls in self.classes():
+            for mth in cls.methods():
+                mths.append(mth)
+
+        return mths
