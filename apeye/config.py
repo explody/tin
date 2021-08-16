fa89e67e7d6cdf28bb0ddba7060fc1b385af2f56
Mike Culbertson <_fsck@itsdown.org>
2021-08-16 08:19:41 -0400

    Building tests. Completely removed the gimpy swagger support.
    I can pull the code back from older revs if I decide to support it again, but there are much better libraries for that, IMHO

--- apeye/config.py
+++ apeye/config.py
@@ -4,23 +4,12 @@ import simplejson as json
 import sys
 import yaml
 
-from apeye.exceptions import ApeyeConfigNotFound
+from apeye.exceptions import ApeyeConfigNotFound, ApeyeError
 
 DEFAULTS = {"scheme": "https", "port": 443}
 
 # We only do JSON APIs right now
 DEFAULT_TYPE = "application/json"
-
-# Paths to check, in order
-#   calling script directory
-#   module directory
-#   user home directory
-confpaths = [
-    os.path.dirname(os.path.realpath(sys.argv[0])),
-    os.path.dirname(os.path.realpath(__file__)),
-    os.path.expanduser("~"),
-    os.path.expanduser("~/.apeye"),
-]
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -34,32 +23,43 @@ class ApeyeConfig(object):
     as object attributes.
 
     Args:
-        conffile (str): Relative or absolute path to the YAML or JSON config file
-        environ (str): Name of the environment to load
+        config_file (str): Relative or absolute path to the YAML or JSON config file
+        environment (str): Name of the environment to load
 
     Attributes:
-        confpaths (list): List of strings of paths in which to find the conffile. Defined above.
+        confpaths (list): List of strings of paths in which to find the config_file. Defined above.
         confpath (str): Final file path of the config file
     """
 
-    def __init__(self, conffile="apeye.yml", environ="development"):
+    def __init__(self, config_file=None, environment=None):
 
-        if "APEYE_ENV" in os.environ:
-            apeye_env = os.environ["APEYE_ENV"]
-        else:
-            apeye_env = environ
+        if config_file is None:
+            if 'APEYE_CONFIG' in os.environ:
+                config_file = os.environ.get('APEYE_CONFIG')
+            else:
+                raise ApeyeError("Config file cannot be None")
 
-        self.confpaths = confpaths
-        self.confpath = self.find_config(conffile)
-        logger.info("Using config: % s Environment: %s" % (self.confpath, environ))
-        fh = open(self.confpath, "rb")
-        conf = yaml.safe_load(fh.read())
+        if environment is None:
+            if 'APEYE_ENV' in os.environ:
+                environment = os.environ.get('APEYE_ENV')
+            else:
+                raise ApeyeError("Environment cannot be None")
 
-        # Add the directory of the file we just loaded to confpaths
-        self.confpaths.append(os.path.dirname(os.path.abspath(self.confpath)))
+        print("ENV", environment)
+        print("FILE", config_file)
 
-        if apeye_env not in conf["environments"]:
-            raise ApeyeError("No such environment is configured: %s" % apeye_env)
+        self.config_file = self.find_config(config_file)
+        self.config_dir = os.path.dirname(self.config_file)
+
+        logger.info("Using config: % s Environment: %s" % (self.config_file, environment))
+
+        conf = self._loadfile(self.config_file)
+
+        if conf is None:
+            raise ApeyeError("Invalid config (empty?)")
+
+        if environment not in conf.get("environments", {}):
+            raise ApeyeError("No such environment is configured: {}".format(environment))
 
         self._api_attrs = conf.get("common", {})
         self.headers = {
@@ -71,36 +71,34 @@ class ApeyeConfig(object):
             else DEFAULT_TYPE,
         }
 
-        if "headers" in conf:
-            self.headers.update(conf["headers"])
+        if "headers" in self._api_attrs:
+            self.headers.update(self._api_attrs["headers"])
 
         self.set("use_session", conf.get("use_session", True))
 
-        env_attrs = conf["environments"][apeye_env]
+        env_attrs = conf["environments"][environment]
 
-        self.api_name = os.path.splitext(os.path.basename(self.confpath))[0]
+        self.api_name = os.path.splitext(os.path.basename(self.config_file))[0]
 
         self._api_attrs.update(env_attrs)
 
+        # Set toplevel keys in the yaml as attributes on this object
         for k, v in self._api_attrs.items():
             setattr(self, k, v)
 
-        # noinspection PyUnresolvedReferences
         if self.credfile:
             self.credentials = self._loadfile(self.find_config(self.credfile))
 
-        # noinspection PyUnresolvedReferences
-        # apifile must be set so don't check, just use it
-        apifile = self.find_config(self.apifile)
-        self.apidata = self._loadfile(apifile)
+        self.apifile = self.find_config(self.apifile)
+        self.apidata = self._loadfile(self.apifile)
 
         if "modelfile" in self._api_attrs:
-            modelfile = self.find_config(self.modelfile)
-            self.models = self._loadfile(modelfile)
+            self.modelfile = self.find_config(self.modelfile)
+            self.models = self._loadfile(self.modelfile)
         else:
             self.models = {}
 
-    def _loadfile(self, fpath):
+    def _loadfile(self, filepath):
         """Parses the conf file as YAML or JSON based on file extension
 
         Arguments:
@@ -109,11 +107,10 @@ class ApeyeConfig(object):
         Returns
             dict: Contents of the file parsed to a dict
         """
-
-        with open(fpath, "rb") as fh:
-            if fpath.endswith(".yml"):
+        with open(filepath, "rb") as fh:
+            if filepath.endswith(".yml"):
                 return yaml.safe_load(fh.read())
-            elif fpath.endswith(".json"):
+            elif filepath.endswith(".json"):
                 return json.loads(fh.read())
 
     def set(self, key, value):
@@ -135,10 +132,14 @@ class ApeyeConfig(object):
         Returns:
             The value of the attribute
         """
-        return getattr(self, key)
+        try:
+            return getattr(self, key)
+        except AttributeError as e:
+            return None
 
     def find_config(self, filename):
-        """Searches the confpaths for the given config file
+        """Takes the given path to a config file, ensures it exists, and returns an absolute
+        path.
 
         Arguments:
             filename (str): The absolute or relative path to the file"
@@ -147,12 +148,15 @@ class ApeyeConfig(object):
         # expanduser here in case someone passed a "~" path
         filename = os.path.expanduser(filename)
 
-        if os.path.isfile(filename):
-            return os.path.abspath(filename)
+        if os.path.isabs(filename):
+            return filename
+        else:
+            if getattr(self, 'config_dir', None):
+                file_in_config_dir = os.path.join(self.config_dir, filename)
+                if os.path.isfile(file_in_config_dir):
+                    return os.path.abspath(file_in_config_dir)
 
-        for path in self.confpaths:
-            confpath = os.path.join(path, filename)
-            if os.path.isfile(confpath):
-                return confpath
+            if os.path.isfile(filename):
+                return os.path.abspath(filename)
 
-        raise ApeyeConfigNotFound(filename, self.confpaths)
+        raise ApeyeConfigNotFound(filename)
---
+++ test/test_config.py
@@ -0,0 +1,101 @@
+import os
+import pytest
+
+from apeye.config import ApeyeConfig
+from apeye.exceptions import *
+
+
+def arg_config_yml():
+    return ApeyeConfig("test/data/api/test.yml", "basic")
+
+
+def arg_config_json():
+    return ApeyeConfig("test/data/api/test.json", "basic")
+
+
+def env_config():
+    os.environ["APEYE_CONFIG"] = "test/data/api/test.yml"
+    os.environ["APEYE_ENV"] = "basic"
+    ac = ApeyeConfig()
+    os.environ.pop("APEYE_CONFIG")
+    os.environ.pop("APEYE_ENV")
+    return ac
+
+
+def test_arg_config_yml():
+    assert type(arg_config_yml()) is ApeyeConfig
+
+
+def test_config_with_env():
+    assert type(env_config()) is ApeyeConfig
+
+def test_arg_config_json():
+    assert type(arg_config_json()) is ApeyeConfig
+
+
+def test_no_config():
+    with pytest.raises(ApeyeError):
+        ApeyeConfig(None, None)
+
+
+def test_no_environment():
+    with pytest.raises(ApeyeError):
+        ApeyeConfig("test/data/api/test.yml", None)
+
+
+def test_bad_config():
+    with pytest.raises(ApeyeConfigNotFound):
+        ApeyeConfig("nosuchfile.yml", "fake")
+
+
+def test_empty_config():
+    with pytest.raises(ApeyeError):
+        ApeyeConfig("test/data/api/empty.yml", "fake")
+
+
+def test_bad_environment():
+    with pytest.raises(ApeyeError):
+        ApeyeConfig("test/data/api/test.yml", "nosuchenv")
+
+
+def test_no_models():
+    ac = ApeyeConfig("test/data/api/test.yml", "no_models")
+    assert ac.models == {}
+
+
+@pytest.mark.parametrize("config", [arg_config_yml(), env_config(), arg_config_json()])
+class TestConfig:
+    def test_config_files(self, config):
+
+        assert config.config_file in [os.path.abspath("test/data/api/test.yml"),
+                                      os.path.abspath("test/data/api/test.json")]
+        assert config.apifile == os.path.abspath("test/data/api/test-api.yml")
+        assert config.modelfile == os.path.abspath("test/data/api/test-models.yml")
+
+    def test_config_dir(self, config):
+        assert config.config_dir == os.path.dirname(
+            os.path.abspath("test/data/api/test.yml")
+        )
+
+    def test_credentials(self, config):
+        assert config.credentials == {'username': 'fakeuser', 'password': 'fakepassword'}
+
+    def test_apiname(self, config):
+        assert config.api_name == 'test'
+
+    def test_environment_values(self, config):
+        print(config.config_file)
+        assert config.host == 'api.example.com'
+        assert config.scheme == 'https'
+        assert config.port == 443
+        assert config.authtype == 'basic'
+        assert config.type == 'application/json'
+        assert config.basepath == '/api'
+        assert config.ssl['verify'] is True
+
+    def test_get(self, config):
+        assert config.get('host') == 'api.example.com'
+        assert config.get('doesntexist') is None
+
+    # def test_environments(self, config):
+    #     assert config.environments == ['basic', 'param', 'header']
