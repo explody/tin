a9ff8c6aaa1321dcfa893a404a4d71d6aed5319c
Mike Culbertson <_fsck@itsdown.org>
2021-09-04 12:05:10 -0400

    cleanup
    Removal of yaml files that wouldn't work correctly any longer
    LICENSE and NOTICE files
    black on some py files
    gitignore on things we don't need to publish

--- setup.py
+++ setup.py
@@ -11,26 +11,30 @@ from apeye.version import VERSION
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 data_files = []
-directories = glob.glob('apis/*')
+directories = glob.glob("apis/*")
 for directory in directories:
-    files = glob.glob(directory+'/*')
-    data_files.append((os.path.expanduser('~/.apeye/'+directory), files))
+    files = glob.glob(directory + "/*")
+    data_files.append((directory, files))
 
 setup(
-    name = "apeye",
-    version = VERSION,
-    author = "Mike Culbertson",
-    author_email = "mikeculbertson@protonmail.com",
-    description = ("Simple REST API wrapper"),
-    license = "BSD",
-    keywords = "",
-    url = "http://pivotal.io",
-    packages=['apeye'],
-    long_description=read('README.md'),
+    name="apeye",
+    version=VERSION,
+    author="Mike Culbertson",
+    author_email="mikeculbertson@protonmail.com",
+    description="Minimal REST API client wrapper around python-requests, with API information defined in YAML",
+    license="Apache",
+    keywords="",
+    url="https://gitlab.com/explody/apeye",
+    packages=["apeye"],
+    long_description=read("README.md"),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
     ],
-    data_files=data_files
+    data_files=data_files,
 )
